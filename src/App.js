e68b699aa8511ad73d2873a7ae07c2ecb49fa75a
MoeFingers <mbzuiter@gmail.com>
2024-03-11 21:22:07 -0600

    played with a few bootstrap components:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
 import './App.css';
+import React from 'react';
+
+import { Button, Nav, Form} from 'react-bootstrap';
+
+// import Button from 'react-bootstrap/Button';
+
 
 
 
 function App() {
+
+  
+
   return (
     <div className="App">
+      <Nav 
+        activeKey="/home"
+        onSelect={(selectedKey) => 
+          {alert(`selected eventKey ${selectedKey}`)}}
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+
+      </Nav>
       <h1>Bootstrap Website</h1>
-     
+      <Button variant="primary">Primary</Button>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+        <Form.Group>
+    <Form.Label>Form Dropdown with favorite movies</Form.Label>
+    <Form.Control as="select">
+      <option>Inception</option>
+      <option>Forrest Gump</option>
+      <option>Shawshank Redemption</option>
+      <option>Spiderman: Into the Spiderverse</option>
+      <option>Avengers: Endgame</option>
+    </Form.Control>
+</Form.Group>
+
+
+      </Form>
+      
     </div>
   );
 }
